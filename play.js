5374e1784e6b2e52b88147334f5072ec8bea6238
Jason J <jj704768703@gmail.com>
2019-07-02 02:38:06 +0800

    用jQuery重写JS

--- play.js
+++ play.js
@@ -1,58 +1,31 @@
-function play() {
-    let myApi = document.getElementById("api");
-    let index = myApi.selectedIndex;
-    let apiValue = myApi.options[index].value;
-    let myText = document.getElementById("mediaUrl").value;
-    if (myText !== '') {
-        document.getElementById("player").src = apiValue + myText;
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();//激活tooltip
 
-        const timer = null;
-        scrollTop(timer);
-        setTimeout(function () {
-            let donate = document.getElementsByClassName("donate")[0];
-            donate.style.opacity = "1";
-            let qrcode = document.getElementById("qrcode");
-            qrcode.style.transform = "scale(1.3)";
-        },2000);
+    $("#formInfo").submit(function (e) {
+        e.preventDefault();//组织表单默认提交
 
-        let donateMobile = document.getElementById("donate-mobile");
-        donateMobile.setAttribute("class","");
-        donateMobile.classList.add("col-sm-8", "col-sm-offset-2", "col-xs-10", "col-xs-offset-1", "donate-mobile", "visible-sm-block", "visible-xs-block");
-        setTimeout(function () {
-            donateMobile.style.opacity = "1";
-        },3000);
-    }else {
-        alert("请输入视频链接");
-    }
-}
-
-function playNewWindow() {
-    let myApi = document.getElementById("api");
-    let index = myApi.selectedIndex;
-    let apiValue = myApi.options[index].value;
-
-    let myText = document.getElementById("mediaUrl").value;
-
-    if (myText !== '') {
-        window.open(apiValue + myText);
-    }else {
-        alert("请输入视频链接");
-    }
-}
-
-function scrollTop(timer) {
-    timer = setInterval(function(){
-        let osTop = document.documentElement.scrollTop || document.body.scrollTop;
-        let iSpeed = Math.ceil(osTop / 50);
-        document.documentElement.scrollTop = document.body.scrollTop = osTop - iSpeed;
-
-        if (osTop === 0) {
-            clearInterval(timer);
+        let mediaUrl = $("#mediaUrl").val();
+        if (mediaUrl !== '') {
+            $("#player").attr("src",$("#api option:selected").val() + mediaUrl);//播放视频
+            $("html, body").animate({scrollTop:0},2000);//回到页面顶部
+            setTimeout(function () {
+                $(".donate:eq(0)").css("opacity",1);//显示捐赠二维码
+                $("#qrcode").css("transform","scale(1.3)");//二维码放大
+            },2000);
+            $("#donate-mobile").removeClass().addClass("col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 donate-mobile visible-sm-block visible-xs-block").delay(3000).animate({opacity:1});//显示移动端捐赠按钮
         }
-    },10)
-}
+    });
 
-function hiddenDonate() {
-    let donate = document.getElementsByClassName("donate")[0];
-    donate.style.opacity = "0";
-}
+    $("#button-new-window").click(function () {
+        let mediaUrl = $("#mediaUrl").val();
+        if (mediaUrl !== '') {
+            window.open($("#api option:selected").val() + mediaUrl);//新窗口播放视频
+        }else {
+            alert("请输入视频链接");
+        }
+    });
+
+    $(".hidden-icon:eq(0)").click(function () {
+        $(".donate:eq(0)").css("opacity",0);//点击隐藏捐赠二维码
+    });
+});
